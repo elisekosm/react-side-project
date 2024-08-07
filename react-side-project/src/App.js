5348149aa62d8e741be710da346b2489f5aab184
Elise Kosmides <46768070+elisekosm@users.noreply.github.com>
2024-08-06 20:35:18 -0400

    Create multiple pages and nav bar

--- react-side-project/src/App.js
+++ react-side-project/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import Header from './Header/Header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header className="Header"/>
+
+      <Navbar />
+
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
