3ecd65490847ad75dcb56258e0c42db7312e0f6c
Elise Kosmides <46768070+elisekosm@users.noreply.github.com>
2024-08-31 14:15:04 -0400

    update nav links

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
 
       <Routes>
-          <Route path="/react-side-project" element={<CatFacts />} />
+          <Route path="/react-side-project/" element={<CatFacts />} />
           <Route path="/react-side-project/about" element={<About />} />
       </Routes>
     </div>
